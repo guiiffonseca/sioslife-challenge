988aba58f6934d0619be9f6a2579d4b33a40bcd0
unknown <guilherme.fonseca@vilaapps.com.br>
2023-09-01 17:26:36 -0300

    configure initial route

---
+++ src/views/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, StatusBar} from 'react-native';
+
+export default function Login(): JSX.Element {
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} backgroundColor="#0cc0df" />
+      <Text>Login</Text>
+    </>
+  );
+}
